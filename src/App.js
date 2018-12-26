4a8dad1a729e393cf525583c515966ea09a4ecab
Lauri Meltti <laurimeltti@gmail.com>
2018-12-26 02:42:17 +0200

    One octave with the new key system

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
 import React, { Component } from 'react';
-import OscSelector from './components/OscSelector'
-import DurationSelector from './components/DurationSelector'
-import Synth from './containers/Synth'
+import OscSelector from './components/OscSelector';
+import DurationSelector from './components/DurationSelector';
+import Synth from './containers/Synth';
 
 class App extends Component {
   state = {
     osc: 'sine',
     soundDuration: '1'
-  }
+  };
 
   handleChange = target => value => {
-    this.setState({[target]: value})
-  }
+    this.setState({ [target]: value });
+  };
 
   render() {
-    const {osc, soundDuration} = this.state
+    const { osc, soundDuration } = this.state;
 
     return (
       <div className="App">
-          <Synth osc={osc} soundDuration={soundDuration}/>
-          <div style={{marginLeft: 10}}>
-          <DurationSelector value={soundDuration} onChange={this.handleChange('soundDuration')}/>
-          <OscSelector selected={osc} onChange={this.handleChange('osc')}/>
-          </div>
+        <Synth osc={osc} soundDuration={soundDuration} />
+        <div style={{ marginLeft: 10 }}>
+          <DurationSelector
+            value={soundDuration}
+            onChange={this.handleChange('soundDuration')}
+          />
+          <OscSelector selected={osc} onChange={this.handleChange('osc')} />
+        </div>
       </div>
     );
   }
--- src/components/Synth.js
+++ src/components/Synth.js
@@ -5,6 +5,7 @@ import DisplayScreen from './DisplayScreen';
 import PowerSwitch from './PowerSwitch';
 import Knobs from './Knobs';
 import BlackKey from './BlackKey';
+import Key from '../containers/Key';
 
 const RootWrapper = styled.div`
   padding: 10px;
@@ -30,25 +31,19 @@ const KeyWrapper = styled.div`
 
 export default () => (
   <RootWrapper>
-    <ControlWrapper>
-      <PowerSwitch />
-      <DisplayScreen />
-      <Knobs />
-    </ControlWrapper>
     <KeyWrapper>
-      <WhiteKey noMargin />
-      <BlackKey />
-      <WhiteKey />
-      <BlackKey />
-      <WhiteKey />
-      <WhiteKey noMargin />
-      <BlackKey />
-      <WhiteKey />
-      <BlackKey />
-      <WhiteKey />
-      <BlackKey />
-      <WhiteKey />
-      <WhiteKey noMargin />
+      <Key variant="white" keyForNote="q" frequency="261.6" noMargin />
+      <Key variant="black" keyForNote="2" frequency="277.2" />
+      <Key variant="white" keyForNote="w" frequency="293.7" />
+      <Key variant="black" keyForNote="3" frequency="311.1" />
+      <Key variant="white" keyForNote="e" frequency="329.6" />
+      <Key variant="white" keyForNote="r" frequency="349.2" noMargin />
+      <Key variant="black" keyForNote="5" frequency="370.0" />
+      <Key variant="white" keyForNote="t" frequency="392.0" />
+      <Key variant="black" keyForNote="6" frequency="415.3" />
+      <Key variant="white" keyForNote="y" frequency="440.0" />
+      <Key variant="black" keyForNote="7" frequency="466.2" />
+      <Key variant="white" keyForNote="u" frequency="493.3" />
     </KeyWrapper>
   </RootWrapper>
 );
