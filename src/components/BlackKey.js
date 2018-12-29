b525cb7bcfb577d56446078ad661c9563ff6cabb
Lauri Meltti <laurimeltti@gmail.com>
2018-12-29 15:15:29 +0200

    Final black key base style

--- src/components/BlackKey.js
+++ src/components/BlackKey.js
@@ -2,21 +2,23 @@ import React from 'react';
 import styled from 'styled-components';
 //background-color: ${({ pressed }) => (pressed ? 'pink' : 'gray')};
 const RootWrapper = styled.div`
-  width: 2em;
-  background: linear-gradient(
-    180deg,
-    rgba(58, 58, 59, 1) 0%,
-    rgba(106, 106, 106, 1) 80%,
-    rgba(40, 40, 40, 1) 100%
-  );
-  border: 1px solid black;
-  height: 8em;
-  z-index: 2;
-  box-shadow: ${({ pressed }) => (pressed ? 'inset 0 0 10px #000000' : '')};
-  margin: 0 0 0 -1em;
+  width: 2.5em;
+  height: 14em;
+
   display: flex;
   justify-content: center;
   align-items: center;
+
+  z-index: 1;
+  margin-left: -1.5em;
+  background-image: linear-gradient(
+    -180deg,
+    #000000 0%,
+    #323232 87%,
+    #818181 99%
+  );
+  box-shadow: ${({ pressed }) => (pressed ? 'inset 0 0 2em #777' : '')};
+  border: 3px solid #000000;
 `;
 
 export default ({ pressed, keyForNote }) => (
