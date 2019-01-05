a2335ca54a10e7b3de32d94e991a758d2cd4a588
Lauri Meltti <laurimeltti@gmail.com>
2019-01-05 12:57:57 +0200

    Sound selector container

---
+++ src/containers/SoundSelector.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import SoundSelector from '../components/SoundSelector';
+
+export default ({ selected, onChange }) => {
+  const sounds = ['sine', 'square', 'sawtooth', 'triangle'];
+  const selectedIndex = sounds.indexOf(selected);
+
+  const handleNextSound = () => {
+    if (selectedIndex + 1 !== sounds.length) {
+      onChange(sounds[selectedIndex + 1]);
+    }
+  };
+
+  const handlePreviousSound = () => {
+    if (selectedIndex > 0) {
+      onChange(sounds[selectedIndex - 1]);
+    }
+  };
+
+  return (
+    <SoundSelector
+      onNextSound={handleNextSound}
+      onPreviousSound={handlePreviousSound}
+      soundIndex={selectedIndex}
+      sounds={sounds}
+    />
+  );
+};
