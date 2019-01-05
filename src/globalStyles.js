904d32a54b6e56966b959986b9197850212a0a65
Lauri Meltti <laurimeltti@gmail.com>
2019-01-05 12:55:42 +0200

    arcade font for the screen

---
+++ src/globalStyles.js
@@ -0,0 +1,8 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  @font-face {
+    font-family: "arcade";
+    src: url('/arcade.ttf') format('truetype');
+  }
+`;
